d10d51df59951cdddc767d9106955e8269bc4cf9
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-12-01 12:19:05 +0000

    inizializzazione

---
+++ app.py
@@ -0,0 +1,21 @@
+#import dellla classe Flask da flask
+from flask import Flask
+
+#inizializziamo flask
+#app rappresenta il nostro server
+app = Flask(__name__)
+
+#il decoratore route definisce gli ENDPOINT
+#"quando siamo alla route "/" richiama il metodo associato"
+@app.route("/")
+def index():
+    #ora una stringa, dopo un json, prossimamente una select da un db
+    data = "ciao mondo"
+    return data
+
+if __name__ == "__main__":
+    #avviamo il server
+    #debug=True permette il riavvio automatico del server
+    #quando modifichiamo il codice
+    #e mostra errori dettagliati in caso di problema.
+    app.run(debug=True)
